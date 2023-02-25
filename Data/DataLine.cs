c1a8c8152ac62aabe231c5658cc2702aec97eeac
Ahmed Chaaben <ahmedchaabene7@gmail.com>
2023-02-25 13:38:54 +0100

    inital

---
+++ Data/DataLine.cs
@@ -0,0 +1,9 @@
+﻿namespace FiverPdfExtractor.Data
+{
+    public class DataLine
+    {
+        public int page { get; set; }   
+        public string Text { get; set; }
+        public string Type { get; set; }
+    }
+}
